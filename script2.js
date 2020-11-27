04d2559b925f9c9183817dc1e084a84d79765b51
Threat Z3R0 <74604677+threat0@users.noreply.github.com>
2020-11-27 18:39:01 +0000

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,18 @@
+    // Write your JS Project  here.
+    $(document).ready(function(){
+        // saisie d'un script d'animation de texte
+	    var threats = new Typed(".threat0", {
+		    strings: ["Hacker", "YouTuber", "Developer", "Web Designer", "Professional security."],
+		    typeSpeed: 100,
+		    backSpeed: 60,
+		    loop: true
+	    });
+        // saisie d'un script d'animation de texte
+	    var  types = new Typed(".threat1", {
+		    strings: ["Hacker", "YouTuber", "Developer", "Web Designer", "Professional security"],
+		    typeSpeed: 90,
+		    backSpeed: 50,
+		    loop: true
+        });
+	  
+    });
