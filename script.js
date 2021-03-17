4cbb32e2db6282e56011e31d51f4305f78549b0c
Threat Z3R0 <74604677+threat0@users.noreply.github.com>
2021-03-17 09:14:56 +0000

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const selectElement = (element) => document.querySelector(element);
-selectElement('.micon').addEventListener( 'click', () => {
-  selectElement('nav').classList.toggle('active');
-});
