2ce3041b6327367574984aa737178a43985d93a8
Threat Z3R0 <74604677+threat0@users.noreply.github.com>
2021-03-22 16:36:20 +0000

    Add files via upload

---
+++ Tutoriel/$/inter_anim.js
@@ -0,0 +1,4 @@
+const lay = document.getElementById("backs");
+setTimeout(function (){
+  lay.classList.add("back");
+}, 5000);
---
+++ Tutoriel/$/loads.js
@@ -0,0 +1,6 @@
+
+const lay = document.getElementById("preload");
+setTimeout(function (){
+  lay.classList.add("quitter");
+  lay.style.transform="scale(1)";
+}, 9000);
