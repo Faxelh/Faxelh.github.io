501febed725d57f51dd35124613be1a1a2066575
Ⓕⓐⓧⓔⓛ <74604677+threat0@users.noreply.github.com>
2021-04-21 19:59:28 +0000

    Add files via upload

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -1,11 +1,11 @@
-var type = new Typed(".hor2", {
+const type = new Typed(".hor2", {
    strings: ["Get in touch on ^1500"],
    typeSpeed: 100,
    backSpeed: 60,
    showCursor: false,
    loop: true
 });
-var types = new Typed(".hor3", {
+const types = new Typed(".hor3", {
    strings: ["Get in touch ^1000"],
    typeSpeed: 100,
    backSpeed: 60,
